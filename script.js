fa546ee96edbe60244f573ea43b54f399b2cfb16
Brandon M <75460543+GelingenTech@users.noreply.github.com>
2023-09-21 08:11:15 -0500

    Testing Win Text

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function checkPassword() {
     if (guess === password) {
         const name = nameInput.value;
         if (name) {
-            message.innerHTML = `Congratulations, ${name}! You guessed the password correctly. Please visit me in the UC and tell me the password to claim your prize! I will be in the UC on Tuesdays and Thursdays from 11-2! If I am not there; please email me at MEYERBJ06@uww.edu`;
+            message.innerHTML = `Congratulations, ${name}! You guessed the password correctly! Please visit me in the UC and tell me the password to claim your prize! I will be in the UC on Tuesdays and Thursdays from 11-2! If I am not there; please email me at MEYERBJ06@uww.edu`;
         } else {
             message.innerHTML = "Please enter your name.";
         }
