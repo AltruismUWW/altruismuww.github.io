394f566a1147867774d406170c2fce3b4c5ccbac
Brandon M <75460543+GelingenTech@users.noreply.github.com>
2023-09-21 08:23:26 -0500

    Removed Name and Updated the welcome text

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-const password = "your_password"; // Replace with your chosen password
+const password = "test"; // Replace with your chosen password
 const nameInput = document.getElementById("name");
 const message = document.getElementById("message");
 
 function checkPassword() {
     const guess = document.getElementById("guess").value;
     if (guess === password) {
-        const name = nameInput.value;
-        if (name) {
-            message.innerHTML = `Congratulations, ${name}! You guessed the password correctly! Please visit me in the UC and tell me the password to claim your prize! I will be in the UC on Tuesdays and Thursdays from 11-2! If I am not there; please email me at MEYERBJ06@uww.edu`;
-        } else {
-            message.innerHTML = "Please enter your name.";
-        }
+   
+	message.innerHTML = `Congratulations, ${name}! You guessed the password correctly! Please visit me in the UC and tell me the password to claim your prize! I will be in the UC on Tuesdays and Thursdays from 11-2! If I am not there; please email me at MEYERBJ06@uww.edu`;
     } else {
         message.innerHTML = "Incorrect password. Try again.";
     }
