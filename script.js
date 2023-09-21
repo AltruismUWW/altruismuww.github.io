de8f6814442770970f730cc77c9bb7888f780933
Brandon M <75460543+GelingenTech@users.noreply.github.com>
2023-09-21 09:50:28 -0500

    Added Image and image text

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-const password = "password"; // Replace with your chosen password
+const password = "warhawk"; // Replace with your chosen password
 const nameInput = document.getElementById("name");
 const message = document.getElementById("message");
 
+const image = document.getElementById("display-image");
+const imageContainer = document.getElementById("image-container");
+
+image.src = "image.jpg";
+
+// Apply CSS styles for aesthetics (you can customize these styles)
+imageContainer.style.textAlign = "center";
+image.style.maxWidth = "100%";
+image.style.height = "auto";
+
 function checkPassword() {
     const guess = document.getElementById("guess").value;
-    if (guess === password) {
+    if (guess === password.ToLowerCase()) {
    
-	message.innerHTML = `Congratulations, ${name}! You guessed the password correctly! Please visit me in the UC and tell me the password to claim your prize! I will be in the UC on Tuesdays and Thursdays from 11-2! If I am not there; please email me at MEYERBJ06@uww.edu ${password}`;
+	message.innerHTML = `Congratulations, ${name}! You guessed the password correctly! Please visit me in the UC and tell me the password to claim your prize! I will be in the UC on Tuesdays and Thursdays from 11AM-1:45PM! If I am not there; please email me at MEYERBJ06@uww.edu`;
     } else {
         message.innerHTML = "Incorrect password. Try again.";
     }
